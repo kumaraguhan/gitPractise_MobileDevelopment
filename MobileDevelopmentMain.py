d3c1cab348f815594d51d614ed35b9881a3e8322
Your Name <you@example.com>
2025-06-27 14:06:33 +0530

    This is the Main Branch at New

---
+++ MobileDevelopmentMain.py
@@ -0,0 +1,28 @@
+class Mobile:
+    def __init__(self, Brand, Model, ManYEAR, Color):
+        self.Brand = Brand
+        self.Model = Model
+        self.ManYEAR = ManYEAR
+        self.Color = Color
+    
+    def Camera(self):
+        self.camera_MP = 2
+        print("Camera is Working")
+    
+    def Call(self):
+        self.call_Quality = 0
+        print("Call function is not working")
+    
+obj1 = Mobile("Samsung", "S20", "12/12/2005", "Red")
+
+print(obj1.Brand)
+
+obj1.Camera()
+
+print(obj1.camera_MP)
+
+# Need Call function to be Working
+# Need Mobile Class to have HD Display Function
+# Need Mobile Class to have WiFi Option
+# Need Mobile Class to have Calculator Options
+# Need Camera to get Upgraded upto 10 MP
